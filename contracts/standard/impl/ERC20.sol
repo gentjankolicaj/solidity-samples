26aebc195aa0fd6a0d3b9e80c660acf4c0e10a95
Gentjan Kolicaj <gentjankolicaj@yahoo.com>
2021-07-14 09:14:17 +0200

    New standard token interfaces added.

---
+++ contracts/standard/impl/ERC20.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.5;
+
+import "../interfaces/IERC20.sol";
+
+contract ERC20 is IERC20 {
+
+}
---
+++ contracts/standard/impl/ERC721.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.5;
+
+import "../interfaces/IERC721.sol";
+
+contract ERC721 is IERC721 {
+
+}
